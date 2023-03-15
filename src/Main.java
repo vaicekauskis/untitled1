2ec91bd4bea4ee65aeab915ff04f00506b6e7146
vaicekauskis <spookylt2@gmail.com>
2023-03-15 12:06:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//      /*Sukurkite 4 kintamuosius, kurie saugotų jūsų vardą, pavardę, gimimo metus ir šiuos metus (nebūtinai tikrus).
+//      Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių ir naudodamas vardo ir pavardės kintamuosius
+//       atspausdintų tokį sakinį :
+//       "Aš esu Vardenis Pavardenis. Man yra XX metai(ų)". */
+
+
+//           /*Naudokite funkcija Math.random(). Sukurkite du kintamuosius ir naudodamiesi funkcija Math.random()
+//           jiems
+//           priskirkite atsitiktines reikšmes nuo 0 iki 4. Didesnę reikšmę padalinkite iš mažesnės. Atspausdinkite
+//           rezultatą jį suapvalinę iki 2 skaičių po kablelio.
+//            */
+
+//
+        int n21 = 0;
+        int n22 = 4;
+        int randomNum = n21 + (int) Math.round(Math.random() * (n21 - n22));
+        System.out.println(randomNum);
+        int randomNum1 = n21 + (int) Math.round(Math.random() * (n22 - n21));
+        System.out.println(randomNum1);
+
+
+//       \* Naudokite funkcija Math.random(). Sukurkite tris kintamuosius ir naudodamiesi funkcija Math.random()
+//        jiems priskirkite atsitiktines reikšmes nuo 0 iki 25. Raskite ir atspausdinkite kintąmąjį
+//        turintį vidurinę reikšmę. */
+//
+//        int nr31 = Math.round() +25
+//        int nr32 = Math.random()
+
+
+//        Įvedami skaičiai -a, b, c –kraštinių ilgiai, trys kintamieji (naudojame int)
+//        (naudokite ​Math.random()​ funkcija nuo 1 iki 10). Parašykite Java programą, kuri nustatytų,
+//                ar galima sudaryti trikampį ir atsakymą atspausdintų.
+
+
+
+
+
+     }
+}
